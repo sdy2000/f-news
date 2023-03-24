edff27d222710ccdbb87524f4a00cfab9c8374dc
sdy2000 <sajjad.darvish.yektayi@gmail.com>
2023-03-24 18:01:59 +0430

    Crate Header and Footer part

--- src/parts/index.js
+++ src/parts/index.js
@@ -1,2 +1,7 @@
 // // // // // Layouts // // // // //
 export { default as HomeLayout } from './website/HomeLayout'
+
+
+// // // // Main Part // // // // 
+export { default as Header } from './website/Header'
+export { default as Footer } from './website/Footer'
--- src/parts/website/HomeLayout.jsx
+++ src/parts/website/HomeLayout.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import {Footer, Header} from '..'
 
 const HomeLayout = () => {
     return (
         <>
+            <Header />
             <Outlet />
+            <Footer />
         </>
     )
 }
